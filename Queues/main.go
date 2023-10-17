26a0adb1f93678ba33601e16b6fdeb6de928c2d3
Rojer Du <rojerdu.dev@gmail.com>
2023-10-16 22:29:29 -0700

    Implement Queues in Golang

--- Queues/main.go
+++ Queues/main.go
@@ -1,1 +1,45 @@
-package Queues
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Queue struct {
+	Elements []string
+}
+
+var ErrEmptyQueue = errors.New("Empty Queue")
+
+func (q *Queue) Enqueue(item string) {
+	q.Elements = append(q.Elements, item)
+}
+
+func (q *Queue) Dequeue(item string) (string, error) {
+	if len(q.Elements) == 0 {
+		return "", ErrEmptyQueue
+	}
+	var firstElement string
+	firstElement, q.Elements = q.Elements[0], q.Elements[1:]
+
+	return firstElement, nil
+}
+
+func (q *Queue) Length() int {
+	return len(q.Elements)
+}
+
+func (q *Queue) Peek() (string, error) {
+	if q.IsEmpty() {
+		return 0, ErrEmptyQueue
+	}
+	return q.Elements[0], nil
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.Elements) == 0
+}
+
+func main() {
+	fmt.Println("Implementing Queues in Golang")
+}
