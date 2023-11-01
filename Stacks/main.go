d9882c655145c381e220d3b68261e24de9982b80
Rojer Du <rojerdu.dev@gmail.com>
2023-10-31 22:26:08 -0700

    Add Push, Pop, Peek, Size, & IsEmpty methods to stack

--- Stacks/main.go
+++ Stacks/main.go
@@ -1,5 +1,7 @@
 package Stacks
 
+import "errors"
+
 // Stack -> LIFO ordered list
 // Add & Get elements at the top of the stack
 
@@ -12,6 +14,29 @@ package Stacks
 
 type Stack struct {
 	elements []any
+}
+
+func (s *Stack) Push(el any) {
+	s.elements = append(s.elements, el)
+}
+
+func (s *Stack) Pop() (el any, err error) {
+	if s.IsEmpty() {
+		err = errors.New("empty stack")
+		return
+	}
+	el = s.elements[len(s.elements)-1]
+	s.elements = s.elements[:len(s.elements)-1]
+	return
+}
+
+func (s *Stack) Peek() (el any, err error) {
+	if s.IsEmpty() {
+		err = errors.New("empty stack")
+		return
+	}
+	el = s.elements[len(s.elements)-1]
+	return
 }
 
 func (s *Stack) Size() int {
